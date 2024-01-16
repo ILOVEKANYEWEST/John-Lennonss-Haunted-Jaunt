44a966811ace2b5d4c6932cced9b2d67ca18f7ac
ILOVEKANYEWEST <113371356+ILOVEKANYEWEST@users.noreply.github.com>
2024-01-16 14:19:51 -0800

    finished

--- john lennons/Assets/Scripts/PlayerMoveMent.cs
+++ john lennons/Assets/Scripts/PlayerMoveMent.cs
@@ -2,30 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMoveMent : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
+    Animator m_Animator;
+    Vector3 m_Movement;
+    public float turnSpeed = 20f;
+    Rigidbody m_Rigidbody;
 
+    Quaternion m_Rotation = Quaternion.identity;
 
-    vector3 m_Movement;
-    // Start is called before the first frame update
     void Start()
     {
-        
+        m_Animator = GetComponent<Animator>();
+        m_Rigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
         m_Movement.Set(horizontal, 0f, vertical);
-        m_Movement.Normlize();
+        m_Movement.Normalize();
 
-        bool hasHorizontalInput = !Mathf.Approximately (horizontal) ,0f;
-        bool hasVeritcalInput = !Mathf.Approximately(horizontal),0f;
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+        m_Animator.SetBool("IsWalking", isWalking);
 
-        bool isWalking = hasHorizontalInput || hasVeritcalInput;
+          Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+        m_Rotation = Quaternion.LookRotation(desiredForward);
 
     }
+
+    void OnAnimatorMove()
+    {
+        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);
+        m_Rigidbody.MoveRotation(m_Rotation);
+    }
+
 }
