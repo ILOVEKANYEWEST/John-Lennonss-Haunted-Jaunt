564a1da303496f54416d77ca4db918554c19b54c
ILOVEKANYEWEST <113371356+ILOVEKANYEWEST@users.noreply.github.com>
2024-01-22 14:17:05 -0800

    5 done

--- john lennons/Assets/Scripts/GameEnding.cs
+++ john lennons/Assets/Scripts/GameEnding.cs
@@ -1,52 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameEnding : MonoBehaviour
-
-
 {
     public float fadeDuration = 1f;
-
+    public float displayImageDuration = 1f;
     public GameObject player;
+    public CanvasGroup exitBackgroundImageCanvasGroup;
+    public CanvasGroup caughtBackgroundImageCanvasGroup;
 
     bool m_IsPlayerAtExit;
-
-    public CanvasGroup exitBackgroundImageCanvasGroup;
-
+    bool m_IsPlayerCaught;
     float m_Timer;
 
-    public float displayImageDuration = 1f;
-    // Start is called before the first frame update
-    void Start()
+    void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject == player)
+        {
+            m_IsPlayerAtExit = true;
+        }
     }
 
-    // Update is called once per frame
+    public void CaughtPlayer()
+    {
+        m_IsPlayerCaught = true;
+    }
+
     void Update()
     {
         if (m_IsPlayerAtExit)
         {
-            EndLevel();
-
+            EndLevel(exitBackgroundImageCanvasGroup, false);
+        }
+        else if (m_IsPlayerCaught)
+        {
+            EndLevel(caughtBackgroundImageCanvasGroup, true);
         }
     }
-     
-    void EndLevel()
+
+    void EndLevel(CanvasGroup imageCanvasGroup, bool doRestart)
     {
         m_Timer += Time.deltaTime;
-        exitBackgroundImageCanvasGroup.alpha = m_Timer / fadeDuration;
+        imageCanvasGroup.alpha = m_Timer / fadeDuration;
+
         if (m_Timer > fadeDuration + displayImageDuration)
         {
-            Application.Quit();
-        }
-    }
-    void OnTriggerEnter(Collider other)
-    {
-        if(other.gameObject == player)
-        {
-            m_IsPlayerAtExit = true;
+            if (doRestart)
+            {
+                SceneManager.LoadScene(0);
+            }
+            else
+            {
+                Application.Quit();
+            }
         }
     }
 }
